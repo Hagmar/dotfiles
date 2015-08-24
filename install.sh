f3917eab122598cfe5892b7f92f209ae8f7a0fd0
Magnus Hagmar <hagmar.magnus@gmail.com>
2015-08-24 14:14:06 +1000

    Automatically downloads Vim plugins

--- install.sh
+++ install.sh
@@ -3,3 +3,24 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ln -sfv "$DIR/vim/.vimrc" ~
+
+###  Vim ###
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+    echo "Error - could not install pathogen" >&2
+    exit 1
+fi
+
+# Gundo
+git clone https://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
+if [ ! -d ~/.vim/bundle/gundo ];then
+    echo "Error - could not install gundo" >&2
+    exit 1
+
+# CtrlP
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+if [ ! -d ~/.vim/bundle/ctrlp.vim ];then
+    echo "Error - could not install CtrlP" >&2
+    exit 1
