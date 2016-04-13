33e9cd5a5b0788a2196fe5c221eff53071bbe28d
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-13 23:25:50 +1000

    Automatically updates vim plugins

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ln -sfv "$DIR/vim/.vimrc" ~
 
 # Pathogen
+echo "Updating Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
     echo "Error - could not install pathogen" >&2
@@ -20,13 +21,19 @@ if [ ! -d ~/.vim/bundle/gundo ];then
         echo "Error - could not install gundo" >&2
         exit 1
     fi
+else
+    echo "Updating Gundo"
+    ( cd ~/.vim/bundle/gundo && git pull )
 fi
 
 # CtrlP
 if [ ! -d ~/.vim/bundle/ctrlp.vim ];then
-    git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+    git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
     if [ ! -d ~/.vim/bundle/ctrlp.vim ];then
         echo "Error - could not install CtrlP" >&2
         exit 1
     fi
+else
+    echo "Updating CtrlP"
+    ( cd ~/.vim/bundle/ctrlp.vim && git pull )
 fi
